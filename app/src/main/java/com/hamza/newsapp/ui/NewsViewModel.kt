413fc68f4cbe4856a74bbf07071b8fec100e5f0d
hamza <aheita3@gmail.com>
2023-09-07 03:52:36 +0300

    handle no internet checking

--- app/src/main/java/com/hamza/newsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/hamza/newsapp/ui/NewsViewModel.kt
@@ -1,8 +1,19 @@
 package com.hamza.newsapp.ui
 
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.ConnectivityManager.TYPE_ETHERNET
+import android.net.ConnectivityManager.TYPE_MOBILE
+import android.net.ConnectivityManager.TYPE_WIFI
+import android.net.NetworkCapabilities.TRANSPORT_CELLULAR
+import android.net.NetworkCapabilities.TRANSPORT_ETHERNET
+import android.net.NetworkCapabilities.TRANSPORT_WIFI
+import android.os.Build
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.hamza.newsapp.App
 import com.hamza.newsapp.models.Article
 import com.hamza.newsapp.models.NewsResponse
 import com.hamza.newsapp.repository.NewsRepository
@@ -10,18 +21,23 @@ import com.hamza.newsapp.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import retrofit2.Response
+import java.io.IOException
 import javax.inject.Inject
 
 @HiltViewModel
-class NewsViewModel @Inject constructor(private val repo: NewsRepository) : ViewModel() {
+class NewsViewModel @Inject constructor(
+    app: Application,
+    private val repo: NewsRepository
+
+) : AndroidViewModel(app) {
 
     val breakingNewsLiveData: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
     var breakingNewsPage = 1
-    var breakingNewsResponse:NewsResponse? = null
+    var breakingNewsResponse: NewsResponse? = null
 
     val searchNewsLiveData: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
     var searchNewsPage = 1
-    var searchNewsResponse:NewsResponse? = null
+    var searchNewsResponse: NewsResponse? = null
 
 
     init {
@@ -29,15 +45,11 @@ class NewsViewModel @Inject constructor(private val repo: NewsRepository) : View
     }
 
     fun getBreakingNews(countryCode: String) = viewModelScope.launch {
-        breakingNewsLiveData.postValue(Resource.Loading())
-        val response = repo.getBreakingNews(countryCode, breakingNewsPage)
-        breakingNewsLiveData.postValue(handleBreakingNewsResponse(response))
+      safeBreakingNewsCall(countryCode)
     }
 
     fun searchNews(searchTerm: String) = viewModelScope.launch {
-        searchNewsLiveData.postValue(Resource.Loading())
-        val response = repo.searchForNew(searchTerm, searchNewsPage)
-        searchNewsLiveData.postValue(handleSearchNewsResponse(response))
+      safeSearchNewsCall(searchTerm)
     }
 
 
@@ -45,14 +57,14 @@ class NewsViewModel @Inject constructor(private val repo: NewsRepository) : View
         if (response.isSuccessful) {
             response.body()?.let { result ->
                 breakingNewsPage++
-                if(breakingNewsResponse==null){
-                    breakingNewsResponse=result
-                }else{
-                    val oldArticle=breakingNewsResponse?.articles
-                    val newArticle=result.articles
+                if (breakingNewsResponse == null) {
+                    breakingNewsResponse = result
+                } else {
+                    val oldArticle = breakingNewsResponse?.articles
+                    val newArticle = result.articles
                     oldArticle?.addAll(newArticle)
                 }
-                return Resource.Success(breakingNewsResponse?:result)
+                return Resource.Success(breakingNewsResponse ?: result)
             }
         }
         return Resource.Error(response.message())
@@ -63,14 +75,14 @@ class NewsViewModel @Inject constructor(private val repo: NewsRepository) : View
         if (response.isSuccessful) {
             response.body()?.let { result ->
                 searchNewsPage++
-                if(searchNewsResponse==null){
-                    searchNewsResponse=result
-                }else{
-                    val oldArticle=searchNewsResponse?.articles
-                    val newArticle=result.articles
+                if (searchNewsResponse == null) {
+                    searchNewsResponse = result
+                } else {
+                    val oldArticle = searchNewsResponse?.articles
+                    val newArticle = result.articles
                     oldArticle?.addAll(newArticle)
                 }
-                return Resource.Success(searchNewsResponse?:result)
+                return Resource.Success(searchNewsResponse ?: result)
             }
         }
         return Resource.Error(response.message())
@@ -85,5 +97,69 @@ class NewsViewModel @Inject constructor(private val repo: NewsRepository) : View
     fun getSavedNews() = repo.getSavedNews()
     fun deleteArticle(article: Article) = viewModelScope.launch {
         repo.deleteArticle(article)
+    }
+
+    private suspend fun safeBreakingNewsCall(countryCode: String) {
+        breakingNewsLiveData.postValue(Resource.Loading())
+        try {
+            if (hasInternetConnection()) {
+                val response = repo.getBreakingNews(countryCode, breakingNewsPage)
+                breakingNewsLiveData.postValue(handleBreakingNewsResponse(response))
+            } else {
+                breakingNewsLiveData.postValue(Resource.Error("No internet connection"))
+            }
+
+        } catch (t: Throwable) {
+            when (t) {
+                is IOException -> breakingNewsLiveData.postValue(Resource.Error("Network Failure"))
+                else -> breakingNewsLiveData.postValue(Resource.Error("ERROR"))
+
+            }
+        }
+    }
+    private suspend fun safeSearchNewsCall(searchTerm: String) {
+        searchNewsLiveData.postValue(Resource.Loading())
+        try {
+            if (hasInternetConnection()) {
+                val response = repo.searchForNew(searchTerm, searchNewsPage)
+                searchNewsLiveData.postValue(handleSearchNewsResponse(response))
+            } else {
+                searchNewsLiveData.postValue(Resource.Error("No internet connection"))
+            }
+
+        } catch (t: Throwable) {
+            when (t) {
+                is IOException -> searchNewsLiveData.postValue(Resource.Error("Network Failure"))
+                else -> searchNewsLiveData.postValue(Resource.Error("ERROR"))
+
+            }
+        }
+    }
+
+    private fun hasInternetConnection(): Boolean {
+        val connectivityManager = getApplication<App>().getSystemService(
+            Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val activeNetwork = connectivityManager.activeNetwork ?: return false
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+            return when {
+                capabilities.hasTransport(TRANSPORT_WIFI) -> true
+                capabilities.hasTransport(TRANSPORT_CELLULAR) -> true
+                capabilities.hasTransport(TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            connectivityManager.activeNetworkInfo?.run {
+                return when (type) {
+                    TYPE_WIFI -> true
+                    TYPE_MOBILE -> true
+                    TYPE_ETHERNET -> true
+                    else -> false
+                }
+            }
+        }
+        return false
     }
 }
